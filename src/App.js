6d561f009dbbe038ba8e9949e31570a1a6ed54bb
DivijJaswal <89727132+DivijJaswal@users.noreply.github.com>
2022-06-29 18:31:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,53 +7,67 @@ import spinner from './spinner.svg';
  
 function App() {
  
-  // State variable to set users source code
+
   const [userCode, setUserCode] = useState(``);
  
-  // State variable to set editors default language
   const [userLang, setUserLang] = useState("python");
  
-  // State variable to set editors default theme
   const [userTheme, setUserTheme] = useState("vs-dark");
  
-  // State variable to set editors default font size
   const [fontSize, setFontSize] = useState(20);
- 
-  // State variable to set users input
+
   const [userInput, setUserInput] = useState("");
- 
-  // State variable to set users output
+  
   const [userOutput, setUserOutput] = useState("");
  
-  // Loading state variable to show spinner
-  // while fetching data
+ 
   const [loading, setLoading] = useState(false);
    
   const options = {
     fontSize: fontSize
   }
  
-  // Function to call the compile endpoint
+ 
   function compile() {
     setLoading(true);
     if (userCode === ``) {
       return
     }
+ let code = userCode;
+    let language = userLang;
+    let input = userInput;
+
+    if (language === "python") {
+      language = "py";
+    }
+
+    let data = {
+      code: code,
+      language: language,
+      input: input,
+    };
+    let config = {
+      method: "post",
+      url: "https://codex-api.herokuapp.com/",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
  
-    // Post request to compile endpoint
-    Axios.post(`https://online-ide2.herokuapp.com/compile`, {
-      code: userCode,
-      language: userLang,
-      input: userInput }).then((res) => {
-      setUserOutput(res.data.output);
-    }).catch((err)=>{
-      setUserOutput(err);
-    }).then(() => {
-      setLoading(false);
-    })
+    Axios(config)
+      .then((res) => {
+        setUserOutput(res.data.output);
+      })
+      .catch((err) => {
+        setUserOutput(err);
+      })
+      .then(() => {
+        setLoading(false);
+      });
   }
- 
-  // Function to clear the output screen
+
   function clearOutput() {
     setUserOutput("");
   }
